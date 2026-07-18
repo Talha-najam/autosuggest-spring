7ceb3cb0c2aa167019372c98d67de9c4dfed1db5
Talha Mohammed Najam <talhanajam07@gmail.com>
2026-07-18 09:28:33 +0300

    more chnages

--- target/classes/static/index.js
+++ target/classes/static/index.js
@@ -1,2 +1,38 @@
 // alert("Hello, welcome to the autosuggest application!");
 console.log("bye from js")
+
+data ={
+    "name": "Talha",
+    "gender": "male",
+}
+
+var users = [
+    {
+        "name": "Talha",
+        "gender": "male",
+         "img": "talha.jpeg"
+        
+        },
+    {
+        "name": "najam",
+        "gender": "male",          
+        "img": "najam.jpeg"
+    }
+]
+var id = 0;
+
+function toggleuser(){
+    id = (id+1) % 2
+    // 1: image
+    var userImage = document.getElementById("user-image");
+    userImage.src = users[id].img;
+
+    // 2: name
+    var userName = document.getElementById("user-name");
+    userName.innerHTML = users[id].name;
+
+    // 3: gender
+    var userGender = document.getElementById("user-gender");
+    userGender.innerHTML = users[id].gender;
+
+}
